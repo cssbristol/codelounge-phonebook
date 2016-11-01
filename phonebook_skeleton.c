4e5b7d6acc3f723e6702826d34b1bdce93561b25
Alessio Zakaria <az15360@my.bristol.ac.uk>
2016-11-01 22:51:17 +0000

    added skeleton and started worksheet

---
+++ phonebook_skeleton.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * Phonebook is a simple phonebook application. Store pairs of names and numbers and then look them up.
+ */
+
+#define NAME_LENGTH 20
+#define NUMBER_LENGTH 20
+#define PHONEBOOK_LENGTH 100
+#define COMMAND_LENGTH 5
+
+/**
+ * A single contact, which has a name and phone number.
+ */
+struct contact {
+};
+
+typedef struct contact Contact;
+
+/**
+ * The phonebook, which stores a number of contacts and keeps track of how many contacts it contains.
+ */
+struct phonebook {
+
+};
+
+typedef struct phonebook Phonebook;
+
+/**
+ * Prompt the user for a name and number, then add them to the phonebook.
+ */
+void add_contact(Phonebook *p) {
+    char *name = malloc((NAME_LENGTH + 1) * sizeof(char));
+    char *number = malloc((NUMBER_LENGTH + 1) * sizeof(char));
+
+}
+
+/**
+ * Prompt the user for a name, then display any matching records in the phonebook.
+ */
+void show_contact(Phonebook *p) {
+    char *name = malloc(20);
+}
+
+/**
+ * Display all records in the phonebook.
+ */
+void display_phonebook(Phonebook *p) {
+    }
+}
+
+/**
+ * Read commands from the user's input and dispatch to the appropriate functions.
+ */
+int main() {
+    Phonebook p;
+    printf("Welcome to the phone book. You can add or lookup contacts. (A/L).\n");
+}
