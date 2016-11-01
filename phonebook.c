611c7c3ac56d3995dd8b1d2237400d8d218e704b
Ross Gardiner <ross-public@gmx.com>
2016-11-01 13:56:41 +0000

    Add more comments, improve CLI

--- phonebook.c
+++ phonebook.c
@@ -6,16 +6,27 @@
  * Phonebook is a simple phonebook application. Store pairs of names and numbers and then look them up.
  */
 
+#define NAME_LENGTH 20
+#define NUMBER_LENGTH 20
+#define PHONEBOOK_LENGTH 100
+#define COMMAND_LENGTH 5
+
+/**
+ * A single contact, which has a name and phone number.
+ */
 struct contact {
-    char name[100];
-    char number[15];
+    char name[NAME_LENGTH];
+    char number[NUMBER_LENGTH];
 };
 
 typedef struct contact Contact;
 
+/**
+ * The phonebook, which stores a number of contacts and keeps track of how many contacts it contains.
+ */
 struct phonebook {
-    int num_contact;
-    Contact contacts[100];
+    int num_contacts;
+    Contact contacts[PHONEBOOK_LENGTH];
 };
 
 typedef struct phonebook Phonebook;
@@ -24,33 +35,33 @@ typedef struct phonebook Phonebook;
  * Prompt the user for a name and number, then add them to the phonebook.
  */
 void add_contact(Phonebook *p) {
-    const int max = 100;
-    char *name = malloc(20);
-    char *number = malloc(20);
+    const int max = NAME_LENGTH + NUMBER_LENGTH + 2;
+    char *name = malloc((NAME_LENGTH + 1) * sizeof(char));
+    char *number = malloc((NUMBER_LENGTH + 1) * sizeof(char));
     char line[max];
-    printf("Name\t Number:\n");
+    printf("Enter new contact details:\n");
+    printf("Name\tNumber\n");
     fgets(line, max, stdin);
     sscanf(line, "%s %s", name, number);
     Contact new_contact;
     strncpy(new_contact.name, name, sizeof(new_contact.name));
     strncpy(new_contact.number, number, sizeof(new_contact.number));
-    p->contacts[p->num_contact] = new_contact;
-    p->num_contact++;
+    p->contacts[p->num_contacts] = new_contact;
+    p->num_contacts++;
 }
 
 /**
  * Prompt the user for a name, then display any matching records in the phonebook.
  */
 void show_contact(Phonebook *p) {
-    const int max = 100;
     char *name = malloc(20);
-    char line[max];
-    printf("Name desired:");
-    fgets(line, max, stdin);
+    char line[PHONEBOOK_LENGTH];
+    printf("Search for name: ");
+    fgets(line, NAME_LENGTH, stdin);
     sscanf(line, "%s", name);
-    for (int i = 0; i < p->num_contact; i++) {
-        if (strcmp(p->contacts[i].name, name) == 0) {
-            printf("Name:%s\tNum:%s\n", name, p->contacts[i].number);
+    for (int i = 0; i < p->num_contacts; i++) {
+        if (strncmp(p->contacts[i].name, name, NAME_LENGTH) == 0) {
+            printf("Name: %s\tNum: %s\n", name, p->contacts[i].number);
             return;
         }
     }
@@ -61,7 +72,7 @@ void show_contact(Phonebook *p) {
  * Display all records in the phonebook.
  */
 void display_phonebook(Phonebook *p) {
-    for (int i = 0; i < p->num_contact; i++) {
+    for (int i = 0; i < p->num_contacts; i++) {
         Contact contact = p->contacts[i];
         printf("Name: %s  Num: %s\n", contact.name, contact.number);
     }
@@ -71,22 +82,25 @@ void display_phonebook(Phonebook *p) {
  * Read commands from the user's input and dispatch to the appropriate functions.
  */
 int main() {
-    Phonebook phonebook;
+    Phonebook p;
+    p.num_contacts = 0;
 
     printf("Welcome to the phone book. You can add or lookup contacts. (A/L).\n");
-    const int max = 10;
-    char line[max], command;
-    fgets(line, max, stdin);
+    char line[COMMAND_LENGTH], command;
+    printf("> ");
+    fgets(line, COMMAND_LENGTH, stdin);
     while (!feof(stdin)) {
         sscanf(line, "%c", &command);
         if (command == 'A') {
-            add_contact(p);
+            add_contact(&p);
         } else if (command == 'L') {
-            show_contact(p);
+            show_contact(&p);
+        } else if (command == 'Q') {
+            break;
         } else {
-            printf("Non valid input\n");
+            printf("Invalid command. Try A or L.\n");
         }
-        printf("Input another command.\n");
-        fgets(line, max, stdin);
+        printf("> ");
+        fgets(line, COMMAND_LENGTH, stdin);
     }
 }
